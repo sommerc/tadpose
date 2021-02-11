25b34afa88458c287076fa71427f6457678c552d
Christoph Sommer <christoph.sommer@ist.ac.at>
2021-02-11 16:56:08 +0100

    rewrite 1/n

--- tadpose/__init__.py
+++ tadpose/__init__.py
@@ -2,8 +2,11 @@ from . import alignment
 from . import analysis
 from . import visu
 
+import numpy
 import pandas
+import matplotlib
 from functools import lru_cache
+
 
 class Tadpole:
     def __init__(self, path, vid_fn, scorer, dlc_config):
@@ -13,6 +16,13 @@ class Tadpole:
         self.dlc_config = dlc_config
 
         self.bodyparts = dlc_config["bodyparts"]
+
+        colorclass = matplotlib.cm.ScalarMappable(cmap="jet")
+        C = colorclass.to_rgba(numpy.linspace(0, 1, len(self.bodyparts)))
+        self.bodypart_colors = (C[:, :3] * 255).astype(numpy.uint8)
+        self.bodypart_color = dict(
+            [(k, v / 255.0) for k, v in zip(self.bodyparts, self.bodypart_colors)]
+        )
 
         self.aligner = None
 
@@ -24,12 +34,12 @@ class Tadpole:
     def aligner(self, ta):
         self._aligner = ta
 
-
-
     @property
     @lru_cache()
     def locations(self):
-        return pandas.read_hdf(f"{self.path}/{self.vid_fn}{self.scorer}.h5")[self.scorer]
+        return pandas.read_hdf(f"{self.path}/{self.vid_fn}{self.scorer}.h5")[
+            self.scorer
+        ]
 
     @property
     @lru_cache()
--- tadpose/visu.py
+++ tadpose/visu.py
@@ -20,80 +20,53 @@ from scipy.ndimage import map_coordinates
 
 coords = ["x", "y"]
 
-def plot_limbs(df, ta, row=None, alpha=0.02, ax=None, contour=False, scatter=True):
+
+def plot_parts(
+    df,
+    tadpole,
+    parts=None,
+    alpha=0.02,
+    ax=None,
+    contour=False,
+    scatter=True,
+    lines=True,
+):
     if ax is None:
-        f, ax = plt.subplots()
+        _, ax = plt.subplots()
 
-    xR, yR = df["LimbR"][coords].to_numpy().T
-    xL, yL = df["LimbL"][coords].to_numpy().T
+    if parts is None:
+        parts = tadpole.bodyparts
 
-    if scatter:
-        ax.plot(xR, yR, ".", color=ta.bodypart_color["LimbR"], alpha=alpha)
-        ax.plot(xL, yL, ".", color=ta.bodypart_color["LimbL"], alpha=alpha)
+    for p in parts:
+        x, y = df[p][coords].to_numpy().T
 
-    if contour:
-        sns.kdeplot(x=xR, y=yR, color=ta.bodypart_color["LimbR"], levels=5)
-        sns.kdeplot(x=xL, y=yL, color=ta.bodypart_color["LimbL"], levels=5)
+        if scatter:
+            fmt = "."
+            if line:
+                fmt = ".-"
+            ax.plot(x, y, fmt, color=tadpole.bodypart_color[p], alpha=alpha)
 
-    x1, x2, y1, y2 = -120, 120, -220, 40
+        if contour:
+            sns.kdeplot(x=x, y=y, color=tadpole.bodypart_color[p], levels=5)
 
-    if row is not None:
-        ax.set_title(f"Limbs: Movie {row.id} ({row.genotype})")
-    ax.set_xlim(x1, x2)
-    ax.set_ylim(y1, y2)
+        if True:
+            ax.plot(
+                numpy.median(x),
+                numpy.median(y),
+                ".",
+                color=tadpole.bodypart_color[p],
+                markersize=8,
+                markeredgecolor="k",
+                markeredgewidth=0.3,
+            )
+
+    x1, x2, y1, y2 = -150, 150, -300, 100
+
+    # ax.set_xlim(x1, x2)
+    # ax.set_ylim(y1, y2)
 
     ax.hlines(0, xmin=x1, xmax=x2, color="gray", linestyle=":")
     ax.vlines(0, ymin=y1, ymax=y2, color="gray", linestyle=":")
-
-    for bi, bp in enumerate(ta.bodyparts):
-        p = df[bp][["x", "y"]].median()
-        ax.plot(
-            p.x, p.y, ".", color=tuple(ta.bodypart_colors[bi] / 255.0), markersize=8, markeredgecolor="k", markeredgewidth=0.3
-        )
-
-    ax.invert_xaxis()
-    ax.set_aspect(1.0)
-    ax.set_xticklabels([])
-    sns.despine(ax=ax)
-
-    plt.tight_layout()
-
-    return ax
-
-
-def plot_tail(df, ta, row, alpha=0.01, ax=None, scatter=True, contour=True):
-    if ax is None:
-        f, ax = plt.subplots()
-
-    xR, yR = df["TailCenter"][coords].to_numpy().T
-    xL, yL = df["TailTip"][coords].to_numpy().T
-
-    if scatter:
-        ax.plot(xR, yR, ".", color=ta.bodypart_color["TailCenter"], alpha=alpha)
-        ax.plot(xL, yL, ".", color=ta.bodypart_color["TailTip"], alpha=alpha)
-
-    # ax.plot(xR, yR, "g-", alpha=0.5)
-    # ax.plot(xL, yL, "r-", alpha=0.5)
-
-    if contour:
-        sns.kdeplot(x=xR, y=yR, color=ta.bodypart_color["TailCenter"], levels=5)
-        sns.kdeplot(x=xL, y=yL, color=ta.bodypart_color["TailTip"], levels=5)
-
-    x1, x2, y1, y2 = -250, 250, -600, 40
-
-    if row is not None:
-        ax.set_title(f"Tail: Movie {row.id} ({row.genotype})")
-    ax.set_xlim(x1, x2)
-    ax.set_ylim(y1, y2)
-
-    ax.hlines(0, xmin=x1, xmax=x2, color="gray", linestyle=":")
-    ax.vlines(0, ymin=y1, ymax=y2, color="gray", linestyle=":")
-
-    for bi, bp in enumerate(ta.bodyparts):
-        p = df[bp][["x", "y"]].median()
-        ax.plot(
-            p.x, p.y, ".", color=tuple(ta.bodypart_colors[bi] / 255.0), markersize=10, markeredgecolor="k", markeredgewidth=0.3,
-        )
 
     ax.invert_xaxis()
     ax.set_aspect(1.0)
