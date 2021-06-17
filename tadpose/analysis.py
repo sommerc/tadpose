f752871e77ffb7e52e63552d442abf2d83a3304d
Christoph Sommer <christoph.sommer@ist.ac.at>
2021-06-17 13:41:55 +0200

    cleanup

--- tadpose/analysis.py
+++ tadpose/analysis.py
@@ -21,14 +21,6 @@ from functools import reduce
 coords = ["x", "y"]
 
 
-def get_valid_alignment_idx(df, max_std=1):
-    return numpy.abs(stats.zscore(df["Center"].y)) < max_std
-
-
-def get_valid_alignment_idx(df, max_std=1):
-    return numpy.abs(stats.zscore(df["Center"].y)) < max_std
-
-
 def get_displacements(df, part, coords=("x", "y")):
     return (df[part][list(coords)].diff(1) ** 2).sum(1, skipna=False).apply(numpy.sqrt)
 
@@ -61,38 +53,6 @@ def filter_likelihood(df, parts, min_likelihood):
     sel = reduce(numpy.logical_and, [df[p].likelihood > min_likelihood for p in parts])
 
     return df[sel]
-
-
-def filter_misdetections_limbs(df, likelihood=0.01, max_displ=50):
-    # by likelihood
-    df_res = df[
-        (df["Center"].likelihood > likelihood)
-        & (df["LimbR"].likelihood > likelihood)
-        & (df["LimbL"].likelihood > likelihood)
-    ]
-
-    # by displacements
-    displL = get_displacements(df_res, "LimbL")
-    displR = get_displacements(df_res, "LimbR")
-    df_res = df_res[(displL < max_displ) & (displR < max_displ)]
-
-    return df_res
-
-
-def filter_misdetections_tail(df, likelihood=0.01, max_displ=150):
-    # by likelihood
-    df_res = df[
-        (df["Center"].likelihood > likelihood)
-        & (df["TailCenter"].likelihood > likelihood)
-        & (df["TailTip"].likelihood > likelihood)
-    ]
-
-    # by displacements
-    displL = get_displacements(df_res, "TailCenter")
-    displR = get_displacements(df_res, "TailTip")
-    df_res = df_res[(displL < max_displ) & (displR < max_displ)]
-
-    return df_res
 
 
 def get_rotation_angle(Rs):
