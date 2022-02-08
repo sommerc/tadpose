b7bcd25795b75c10be65352ee57c16f32c4d4223
Christoph Sommer <christoph.sommer@ist.ac.at>
2022-02-08 15:19:11 +0100

    removed unused funcs

--- tadpose/analysis.py
+++ tadpose/analysis.py
@@ -31,12 +31,12 @@ def get_displacements(df, part, coords=("x", "y")):
     return (df[part][list(coords)].diff(1) ** 2).sum(1, skipna=False).apply(numpy.sqrt)
 
 
-def get_speed(df, part, coords=("x", "y"), per="frame"):
-    displ = get_displacements(df, part, coords)
-    dt = df["Time"][per].diff(1)
-    res = displ / dt
-    res.iloc[0] = res.iloc[1]
-    return res
+# def get_speed(df, part, coords=("x", "y"), per="frame"):
+#     displ = get_displacements(df, part, coords)
+#     dt = df["Time"][per].diff(1)
+#     res = displ / dt
+#     res.iloc[0] = res.iloc[1]
+#     return res
 
 
 def ego_speeds(tadpole, parts=None):
@@ -65,22 +65,22 @@ def speeds(tadpole, parts=None):
     return pandas.DataFrame(speeds, columns=parts)
 
 
-def get_angle(df_flt_tail, part1="TailTip", part2="TailCenter"):
-    v0 = df_flt_tail[part1][coords].to_numpy() - df_flt_tail[part2][coords].to_numpy()
-    v1 = df_flt_tail[part2][coords].to_numpy()
+# def get_angle(df_flt_tail, part1="TailTip", part2="TailCenter"):
+#     v0 = df_flt_tail[part1][coords].to_numpy() - df_flt_tail[part2][coords].to_numpy()
+#     v1 = df_flt_tail[part2][coords].to_numpy()
 
-    v0 = v0 / numpy.linalg.norm(v0, axis=1)[:, None]
-    v1 = v1 / numpy.linalg.norm(v1, axis=1)[:, None]
+#     v0 = v0 / numpy.linalg.norm(v0, axis=1)[:, None]
+#     v1 = v1 / numpy.linalg.norm(v1, axis=1)[:, None]
 
-    c = (v0 * v1).sum(1)
-    angles = numpy.arccos(numpy.clip(c, -1, 1))
-    return numpy.rad2deg(angles)
+#     c = (v0 * v1).sum(1)
+#     angles = numpy.arccos(numpy.clip(c, -1, 1))
+#     return numpy.rad2deg(angles)
 
 
-def filter_likelihood(df, parts, min_likelihood):
-    sel = reduce(numpy.logical_and, [df[p].likelihood > min_likelihood for p in parts])
+# def filter_likelihood(df, parts, min_likelihood):
+#     sel = reduce(numpy.logical_and, [df[p].likelihood > min_likelihood for p in parts])
 
-    return df[sel]
+#     return df[sel]
 
 
 def get_rotation_angle(Rs):
