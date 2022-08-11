7a27ff5a071ab210fe4887533498bfc8df27e7fc
Christoph Sommer <christoph.sommer@ist.ac.at>
2022-08-11 12:33:16 +0200

    added angles for specific frames option

--- tadpose/analysis.py
+++ tadpose/analysis.py
@@ -36,8 +36,9 @@ def speeds(tadpole, parts=None):
     return pd.DataFrame(speeds, columns=parts)
 
 
-def angles(tad, part_tuple1, part_tuple2, win=5, track_idx=0):
-    elocs = tad.ego_locs(track_idx=track_idx).copy()
+def angles(tad, part_tuple1, part_tuple2, win=5, track_idx=0, frames=None):
+    frames = tad._check_frames(frames)
+    elocs = tad.ego_locs(track_idx=track_idx)[frames].copy()
 
     elocs[..., 0] *= -1
 
